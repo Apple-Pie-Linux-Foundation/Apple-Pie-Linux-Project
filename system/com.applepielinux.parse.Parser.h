e5fb25131bb7e17d092e0f8a07f8ba0fab7d0ae1
Boonk3312 <theboonk7@hotmail.com>
2024-02-26 12:50:59 -0800

    Add files via upload
    Signed-off-by: Boonk3312 <theboonk7@hotmail.com>

---
+++ system/com.applepielinux.parse.Parser.h
@@ -0,0 +1,24 @@
+void Parser() {
+
+	// control lines
+        #define line_0 = a20(int) ++Line(a20);
+        #define line_1 = a24(int) ++Line(a24);
+        #define line_2 = a28(int) ++Line(a24);
+	// parse
+	#ifdef CONTROL_0
+	public static void parse() {
+
+                #define input = "Input"
+		parse++(input)::Input == {
+
+		}
+
+		if input(nullptr == 1) {
+
+			void ++NullCheck() {
+				break;
+			}
+		}
+	}
+        #endif
+}
