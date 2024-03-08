34a897bd86906744805abb513325806c26e2fc57
Boonk3312 <theboonk7@hotmail.com>
2024-03-08 13:17:54 -0800

    Update CollectConcurrent.java
    Signed-off-by: Boonk3312 <theboonk7@hotmail.com>

--- system/java/JavaRuntimeEnvironment/jre/Concurrent/CollectConcurrent.java
+++ system/java/JavaRuntimeEnvironment/jre/Concurrent/CollectConcurrent.java
@@ -1,4 +1,4 @@
-package applepielinux.system.java.JavaRuntimeEnvironment.jre.Concurrent.CollectConcurrent;
+package org.applepielinux.system.java.JavaRuntimeEnvironment.jre.Concurrent.CollectConcurrent;
 
 import java.*;
 import java.io.nio.*;
@@ -25,12 +25,43 @@ public class CollectConcurrent {
               continue;
             }
 
+            private class get() {
+
+                  Resource = public static void res(String, args[]) {
+                        ResourceProperty resourceproperty;
+                        return res;
+                  
+                        res.Op = {
+                              return 1;
+                              continue;
+                        }
+                  
+                        res.Nop = {
+                              return 0;
+                              break;
+                        }
+                  
+                        if (res.Nop != true) {
+                             return res.Nop;      
+                        } else {
+                             return res.Op;
+                        } else {
+                             return 0;
+                        } else {
+                             return 1;
+                        }
+                  } 
+            }
             @Override
 
             int Concurrent = struct list(Concurrent) {
                   private static void Concurrent(String, args[]) {
                         int ConcurrentNull(null) = null;
                         int ConcurrentInt(int) = int;
+
+                        public ConcurrentInt(String Int = "Integer(null)", get(Resource) {
+                              
+                        }
                   }
             }
       }
