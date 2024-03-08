2bd90245c2dba194c9157ed5fbbb4a8f076824ce
Boonk3312 <theboonk7@hotmail.com>
2024-03-08 14:09:43 -0800

    Create IntConcurrent.java
    Signed-off-by: Boonk3312 <theboonk7@hotmail.com>

---
+++ system/java/JavaRuntimeEnvironment/jre/Concurrent/IntConcurrent.java
@@ -0,0 +1,31 @@
+package org.applepielinux.system.java.JavaRuntimeEnvironment.jre.Concurrent.IntConcurrent;
+
+import org.applepielinux.system.java.JavaRuntimeEnvironment.jre.Concurrent.CollectConcurrent;
+
+public class IntConcurrent {
+      public static void main(String, args[]) {
+            int concurrentInteger1 = 0x00A235;
+            int concurrentInteger2 = 0x86FC80;
+
+            struct timestamp {
+                   TIMESTAMP(int, concurrent:null) {
+                           //#// Time                    @param concurrent.timestamp
+                           //#// Null                    @param concurrent.timestamp.null
+                           //#// LinuxTimestampInt       @param concurrent.timestamp.linuxtimestampint
+                           return timestamp;
+                           final int TimestampOutput(extends Timestamp) {
+                                @Override
+                                Collection<? extends Timestamp> timestamp();
+
+                                @Override
+                                default Iterable<? extends final TimestampOutput> __Timestamp_Output__() {
+                                    return this.TimestampOutput();
+                                }
+                           } 
+                      }
+                 }
+            }
+      }
+
+      return Int;
+}
