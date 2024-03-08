60933f8f0063c28490865626a1383d5e44d852ac
Boonk3312 <theboonk7@hotmail.com>
2024-03-08 12:46:11 -0800

    Create CollectConcurrent.java
    Signed-off-by: Boonk3312 <theboonk7@hotmail.com>

---
+++ system/java/JavaRuntimeEnvironment/jre/Concurrent/CollectConcurrent.java
@@ -0,0 +1,37 @@
+package applepielinux.system.java.JavaRuntimeEnvironment.jre.Concurrent.CollectConcurrent;
+
+import java.*;
+import java.io.nio.*;
+import java.io.File.*;
+import java.io.*;
+
+public class CollectConcurrent {
+
+      public static void main(String, args[]) {
+
+            // Check for null
+        
+            if (!= null) {
+                break;
+            } else {
+                continue;
+            }
+
+            // Check for division by zero
+        
+            if (!= DividedByZeroException) {
+              break;
+            } else {
+              continue;
+            }
+
+            @Override
+
+            int Concurrent = struct list(Concurrent) {
+                  private static void Concurrent(String, args[]) {
+                        int ConcurrentNull(null) = null;
+                        int ConcurrentInt(int) = int;
+                  }
+            }
+      }
+}
