9ddefdf0bd35f24f5b9a3ff1f03ea9236dd3c511
Boonk3312 <theboonk7@hotmail.com>
2024-04-03 10:48:53 -0700

    Update File.java
    Signed-off-by: Boonk3312 <theboonk7@hotmail.com>

--- system/java/JavaRuntimeEnvironment/file/jre/File.java
+++ system/java/JavaRuntimeEnvironment/file/jre/File.java
@@ -1,5 +1,35 @@
-public class File() {
-     public static void main(String, args[]) {
-           System.out.printIn("            ");
-     }
+package jre.io.File;
+
+import java.io.File; // Importing the File class from java.io package
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating a new File object representing a file or directory path
+        File myFile = new File("/path/to/your/file.txt");
+        
+        // Performing various operations on the File object
+        if (myFile.exists()) {
+            System.out.println("The file exists.");
+        } else {
+            System.out.println("The file does not exist.");
+        }
+        
+        if (myFile.isDirectory()) {
+            System.out.println("The file is a directory.");
+        } else {
+            System.out.println("The file is not a directory.");
+        }
+        
+        if (myFile.canRead()) {
+            System.out.println("You have read permission.");
+        } else {
+            System.out.println("You don't have read permission.");
+        }
+        
+        if (myFile.length() > 0) {
+            System.out.printf("The size of the file is %d bytes.", myFile.length());
+        } else {
+            System.out.println("The file has zero length.");
+        }
+    }
 }
