072268312638b80a2648bda0dbe905d9d4c20381
Boonk3312 <theboonk7@hotmail.com>
2024-04-21 09:44:41 -0700

    Update recovery_ui.c
    Signed-off-by: Boonk3312 <theboonk7@hotmail.com>

--- system/recovery/recovery_ui.c
+++ system/recovery/recovery_ui.c
@@ -7,4 +7,11 @@
 class recovery_ui() {
     RecoveryUI.ScreenRes = 800, 600;
     RecoveryUI.ScreenColorSpace = ColorSpace.(open_Image("srgb_map.png"));
+    RecoveryUI.(NewObject(Type("Button", size(52, 48), TextColor(TextColor.ColorIndex.WHITE), BackgroundColor(BackgroundColor.ColorIndex.HALF_DARK_AQUA)) => {
+        Button.DisplayName("Restart the computer", FONT(Arial));
+        Button.DoesOnPressed().Activity() => {
+            Kernel.utilities(command{shell.cmd<"reboot">});
+            Kernel.utilities(reboot)::Normal<args(none)>
+        };
+    }
 }
