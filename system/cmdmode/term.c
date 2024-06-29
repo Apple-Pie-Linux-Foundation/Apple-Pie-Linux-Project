9c3aa039165468dde10ebdef929d49be2425c497
Boonk3312 <theboonk7@hotmail.com>
2024-06-29 08:21:02 -0700

    Create term.c
    Signed-off-by: Boonk3312 <theboonk7@hotmail.com>

---
+++ system/cmdmode/term.c
@@ -0,0 +1,16 @@
+#include "bootmsgs.c"
+#include "intel_iPXE/iso_ipxe.iso"
+#include "isoload.c"
+
+int term() {
+   printf(*bootmsgs, "-- System panic");
+   printf(*bootmsgs, "Booted into cmdmode due to: SystemException: system panic");
+
+   private term_prompt = "systemcmdmode";
+   private term_baud_rate = 256000;
+   private term_processor_bits = 32;
+   private term_memory_speed = 128000;
+  
+   printf("Intel iPXE build 34 v2.1 Intel Base Code ROM, Copyright Intel (C) 2000, 2010");
+   isoload("intel_iPXE/iso_ipxe.iso", iPXE_BootFirmware_ISO_Intel);
+}
