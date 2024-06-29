2c192b1cb96f2392841ef9154896f010ed6d4295
Boonk3312 <theboonk7@hotmail.com>
2024-06-29 07:49:29 -0700

    Create bootmsgs.c
    Signed-off-by: Boonk3312 <theboonk7@hotmail.com>

---
+++ system/cmdmode/bootmsgs.c
@@ -0,0 +1,86 @@
+#include <systemc.h>
+#include <string>
+
+using namespace sc_core;
+using namespace std;
+
+SC_MODULE(IDE) {
+    // Declare inputs and outputs if needed
+};
+
+SC_MODULE(PCIx) {
+    // Declare inputs and outputs if needed
+};
+
+SC_MODULE(ESATA) {
+public:
+    sc_in<bool> clk;
+    sc_out<std::string> path;
+
+    void write(const std::string& path) {
+        // Write logic goes here
+    }
+
+    void read_permissions() {
+        // Read permissions logic goes here
+    }
+
+    void thread_process() {
+        wait();
+
+        write(/* Your path */);
+        read_permissions();
+    }
+};
+
+SC_MODULE(ETH) {
+public:
+    sc_in<bool> clk;
+    sc_in<unsigned char*> ethData;
+    sc_out<std::string> encryptionKey;
+
+    void thread_process() {
+        wait();
+
+        // Replace this line with your actual Ethernet signal transmission logic
+        sendEthernetSignal(ethData, encryptionKey);
+    }
+};
+
+SC_MODULE(bootmsgs) : sc_module("bootmsgs") {
+    QUIETELSECLASS(bootmsgs, "bootmsgs");
+
+    SC_HAS_PROCESS(bootmsgs);
+
+    bootmsgs(sc_module_name name) : sc_module(name) {
+        IDE ide("ide");
+        PCIx pci("/PCI@0xC0000471e/PCIe_X2_1.0");
+        ESATA esata("esata");
+        ETH eth("eth");
+
+        sensitive << clk.pos();
+
+        idesign(ide, pci, esata);
+        idesign(pci, esata);
+        idesign(esata, eth);
+
+        clk.register_close(_clock());
+
+        printf("0x%x, %p\n", 0xED4401EU + static_cast<unsigned int>(System::getVersion()), &System::driverManagementApplication);
+        printf("0x%x, %p\n", 0xFF18103U, &System::driverManagementApplication);
+        printf("0x%x, %p\n", 0xA1835CBU, &System::bootManager);
+        printf("0x%x, %p\n", 0xC000014U, &System::IPXE);
+        printf("0x%x, %p\n", 0x2FFF27BU, &System::bootOptions);
+
+        eth.write("AgoyoaFY6TTAOY887sTSFTakujmhGAKH.ksgLHSlauhsfoFAYKGCgcVJHVulfugCgcJGCHGKcgh/.,,/.><M$?@N$M@#n4/23n$<M$N#2/.,me.,M?><>,mS/aMS:las;amS:?<Y!@Pu#H!:kb3?!@mne !@?#>m");
+};
+
+int main(int argc, char **argv) {
+    sc_init(argc, argv);
+
+    bootmsgs bootMsgs("bootmsgs");
+
+    sc_start(100, SC_NS);
+
+    return sc_finish();
+}
